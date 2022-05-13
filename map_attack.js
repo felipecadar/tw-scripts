ea8db6c4f2c7ea3742bccfa39a5a5b0034d02aec
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2022-05-13 10:28:07 -0300

    update

--- map_attack.js
+++ map_attack.js
@@ -2,8 +2,30 @@ javascript:"use strict";
 
 $(document).ready(function () {
 
+    const tempo_campo = {
+        "spear": 9,
+        "sword": 12,
+        "axe": 9,
+        "spy": 5,
+        "light": 5,
+        "heavy": 6,
+        "ram": 16,
+        "catapult": 16,
+        "knight": 5,
+        "snob": 30,
+    }
+
     const attack_config = {
-        "spear":0,
+        "spear": 0,
+        "sword": 0,
+        "axe": 0,
+        "spy": 5,
+        "light": 10,
+        "heavy": 0,
+        "ram": 0,
+        "catapult": 0,
+        "knight": 0,
+        "snob": 0,
     }
 
     function delay(time) {
@@ -23,16 +45,38 @@ $(document).ready(function () {
         await delay(400);
         mp_att.click()
         await delay(600);
-        unit_input_spear.value = 0
-        unit_input_sword.value = 0
-        unit_input_axe.value = 0
-        unit_input_spy.value = 10
-        unit_input_light.value = 10
-        unit_input_heavy.value = 0
-        unit_input_ram.value = 0
-        unit_input_catapult.value = 0
-        unit_input_knight.value = 0
-        unit_input_snob.value = 0
+
+        if(attack_config.spear > 0){
+            unit_input_spear.value = attack_config.spear
+        }
+        if(attack_config.sword > 0){
+            unit_input_sword.value = attack_config.sword
+        }
+        if(attack_config.axe > 0){
+            unit_input_axe.value = attack_config.axe
+        }
+        if(attack_config.spy > 0){
+            unit_input_spy.value = attack_config.spy
+        }
+        if(attack_config.light > 0){
+            unit_input_light.value = attack_config.light
+        }
+        if(attack_config.heavy > 0){
+            unit_input_heavy.value = attack_config.heavy
+        }
+        if(attack_config.ram > 0){
+            unit_input_ram.value = attack_config.ram
+        }
+        if(attack_config.catapult > 0){
+            unit_input_catapult.value = attack_config.catapult
+        }
+        if(attack_config.knight > 0){
+            unit_input_knight.value = attack_config.knight
+        }
+        if(attack_config.snob > 0){
+            unit_input_snob.value = attack_config.snob
+        }
+
         target_attack.click()
         await delay(400);
         if (typeof troop_confirm_submit  !== 'undefined'){
@@ -52,7 +96,6 @@ $(document).ready(function () {
     var x = 0;
     var altAldTempo = aleatorio(180000, 300000);
     //var altAldTempo = aleatorio(30000,60000);
-    var villagesToSkip = []; // add the villages id as string to the array
 
     function aleatorio(superior, inferior) {
         var numPosibilidades = superior - inferior;
@@ -105,7 +148,7 @@ $(document).ready(function () {
     var target_list = []
     var tgt_tribes = []
     var tgt_tribes_str = []
-    var under_attack = []
+    var under_attack = [11585]
     
     for(var idx in TWMap.allyRelations){
         let data = TWMap.allyRelations[idx]
@@ -169,6 +212,10 @@ $(document).ready(function () {
                 continue
             }
 
+            if(tribes[tgt_village.ally_id] && parseInt(tribes[tgt_village.ally_id].points.split('.').join("")) >= 8*maxPts){
+                continue
+            }
+
             // Slip Friends
             if (friends[tgt_village.owner]) {
                 continue
@@ -179,10 +226,11 @@ $(document).ready(function () {
             if (tribes[tgt_village.ally_id] != null){
                 if (!tgt_tribes.includes(tgt_village.ally_id)){
                     tgt_tribes.push(tgt_village.ally_id)
-                    tgt_tribes_str.push(tribes[tgt_village.ally_id].name + " id: " + tgt_village.ally_id)
+                    tgt_tribes_str.push(tribes[tgt_village.ally_id].name + " pts: " + tribes[tgt_village.ally_id].points + " id: " + tgt_village.ally_id)
                 }
             }
             
+            console.log(tgt_village)
             // if(target_list.length >= 3){
             //     break
             // }
